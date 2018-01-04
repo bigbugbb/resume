96e717ae0519412ab40aef9f1b5b144a984d21ae
bigbugbb <bigbugbb@gmail.com>
2018-01-05 02:47:47 +0800

    made my resume website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  private isMenuOpened = false;
+
+  downloadResume(event) {
+    event.preventDefault();
+  }
+
+  showMenu(event) {
+    this.isMenuOpened = !this.isMenuOpened;
+    event.preventDefault();
+  }
 }
